10fd4a653fd4ee64c158fcb221a122cf8d8335e9
Scott Byrns <contact@scottbyrns.com>
2011-08-22 16:06:49 -0600

    Adding util package and user account stubs.

---
+++ util/src/main/java/com/ideastobettertheworld/entities/Sex.java
@@ -0,0 +1,20 @@
+package com.ideastobettertheworld.entities;
+
+/**
+ * Enumeration of different sexes.
+ */
+public enum Sex
+{
+    /**
+     * Male sex.
+     */
+    MALE,
+    /**
+     * Female sex.
+     */
+    FEMALE,
+    /**
+     * Other sex.
+     */
+    OTHER
+}
---
+++ util/src/main/java/com/ideastobettertheworld/entities/User.java
@@ -0,0 +1,71 @@
+package com.ideastobettertheworld.entities;
+
+/**
+ * User Object
+ */
+public class User
+{
+    private long userId;
+    private Username username;
+    private Sex sex;
+
+    /**
+     * Get the users id.
+     *
+     * @return The users id.
+     */
+    public long getUserId ()
+    {
+        return userId;
+    }
+
+    /**
+     * Set the users id.
+     *
+     * @param userId The users id.
+     */
+    public void setUserId (long userId)
+    {
+        this.userId = userId;
+    }
+
+    /**
+     * Get the users username.
+     *
+     * @return The users username.
+     */
+    public Username getUsername ()
+    {
+        return username;
+    }
+
+    /**
+     * Set the users username.
+     *
+     * @param username The users username.
+     */
+    public void setUsername (Username username)
+    {
+        this.username = username;
+    }
+
+    /**
+     * Get the users sex.
+     *
+     * @return The users sex.
+     */
+    public Sex getSex ()
+    {
+        return sex;
+    }
+
+    /**
+     * Set the users sex.
+     *
+     * @param sex The users sex.
+     */
+    public void setSex (Sex sex)
+    {
+        this.sex = sex;
+    }
+}
