f1885a77b3501c4041cabf23cc5cb14380fcf418
Scott Byrns <contact@scottbyrns.com>
2011-09-17 00:37:02 -0600

    Adding hibernate POC for ideas.

--- ServerAPI/src/main/java/com/ideas/api/server/services/IdeaService.java
+++ ServerAPI/src/main/java/com/ideas/api/server/services/IdeaService.java
@@ -1,9 +1,12 @@
 package com.ideas.api.server.services;
 
 import com.ideas.entities.ideas.Idea;
+import com.ideas.utils.HibernateUtil;
+import org.hibernate.Session;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
 /**
  * Service to provide a crud interface to ideas.
@@ -14,5 +17,46 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class IdeaService extends BaseService
 {
+
+
+    /**
+     * Create a document.
+     *
+     * @return The document.
+     *
+     * @todo Use unique ID from mongo.
+     * @todo Handle exceptions
+     * @todo Handle null ideas.
+     */
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String create(String data)
+    {
+
+
+        try
+        {
+            Idea idea = getObjectMapper().readValue(data, Idea.class);
+
+            data = getObjectMapper().writeValueAsString(idea);
+
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+            session.beginTransaction();
+
+            session.save(idea);
+            session.getTransaction().commit();
+
+            HibernateUtil.getSessionFactory().close();
+
+            return data;
+        }
+        catch (Exception e)
+        {
+            return e.getLocalizedMessage();
+        }
+
+    }
 
 }
