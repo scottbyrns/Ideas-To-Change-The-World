cdc9281d13d8e669940463c4c1e6637b2b574639
Scott Byrns <contact@scottbyrns.com>
2011-09-18 22:44:01 -0600

    Adding prototype page.

---
+++ searchResults.js
@@ -0,0 +1,15 @@
+	LiveWidgets.addWidget({
+    	name: 'search-results',
+    	model: {
+
+    	},
+    	controller: {
+
+    	},
+    	constructor: function () {
+
+    	},
+    	reinit: function () {
+
+    	}
+    });
